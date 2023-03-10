4405b0e1782665f3968a8504ff0663d6eddcb6cc
Martonnvok <martonsom@gmail.com>
2023-03-10 11:02:24 +0100

    utolsó commit

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 
 window.addEventListener("load", function () {
     const KEPEK = document.querySelectorAll("article");
-    let lista=["kep.jpg","kep2.png","kep3.png","kopasz.jpg","eznyoc.gif","thefoodisfood.png","laza.png","thanospofa.png"]
-    
+    let lista = ["kep.jpg", "kep2.png", "kep3.png", "kopasz.jpg", "eznyoc.gif", "thefoodisfood.png", "laza.png", "thanospofa.png"]
+
     for (let index = 0; index < lista.length; index++) {
-        KEPEK[0].innerHTML += "<div><img src='"+lista[index]+"' alt='1' ></div>";
-        
+        KEPEK[0].innerHTML += "<div><img src='" + lista[index] + "' alt='1' ></div>";
+
     }
-    const kepELEM = document.querySelectorAll("article img");
+    const KEPELEM = document.querySelectorAll("article img");
 
     let mainkep = document.querySelectorAll("section img");
     let fokep = document.querySelectorAll("section div img");
 
     for (let index = 0; index < lista.length; index++) {
-        kepELEM[index].addEventListener("click", function () {
+        KEPELEM[index].addEventListener("click", function () {
 
             fokep[0].src = lista[index];
         })
@@ -21,18 +21,28 @@ window.addEventListener("load", function () {
     }
 
 
-    const balElem=document.querySelectorAll(".bal");
-    const jobbElem=document.querySelectorAll(".jobb");
+    const BALELEM = document.querySelectorAll(".bal");
+    const JOBBELEM = document.querySelectorAll(".jobb");
     let aktElem = 0;
-    jobbElem[0].addEventListener("click",function(){
+    JOBBELEM[0].addEventListener("click", function () {
         aktElem++;
-        fokep[0].src=lista[aktElem];
+        fokep[0].src = lista[aktElem];
+        console.log(aktElem)
+        if (aktElem > 6) {
+            aktElem = 0;
+        }
     })
 
-    balElem[0].addEventListener("click",function(){
+    BALELEM[0].addEventListener("click", function () {
         aktElem--;
-        fokep[0].src=lista[aktElem];
+        fokep[0].src = lista[aktElem];
+
+        if (aktElem < lista.length) {
+            aktElem = lista.length;
+        }
     })
+
+
 
 });
 
